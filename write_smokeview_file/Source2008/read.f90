a5ff89a8af43b8d0c609be23f45ae04b379195cd
godisreal <wp2204@gmail.com>
2025-06-26 20:37:41 +0800

    Update source code of fds2008: Modify read.f90 and remove main_mpi.f90
    Signed-off-by: godisreal <wp2204@gmail.com>

--- write_smokeview_file/Source2008/read.f90
+++ write_smokeview_file/Source2008/read.f90
@@ -52,10 +52,10 @@ CALL GET_INPUT_FILE
 
 IF (FN_INPUT(1:1)==' ') THEN
    IF (MYID==0) THEN
-      WRITE(LU_ERR,'(/A)') "Fire Dynamics Simulator"
+      WRITE(LU_ERR,'(/A)') "Generate SMV Script from Fire Dynamics Simulator"
       IF (.NOT.PARALLEL)   WRITE(LU_ERR,'(/A,A,A)') "Version: ",TRIM(VERSION_STRING)," Serial"
       IF (PARALLEL)        WRITE(LU_ERR,'(/A,A,A)') "Version: ",TRIM(VERSION_STRING)," Parallel"
-      WRITE(LU_ERR,'(A,I4)') "SVN Revision Number: ",SVN_REVISION_NUMBER
+      !WRITE(LU_ERR,'(A,I4)') "SVN Revision Number: ",SVN_REVISION_NUMBER
       WRITE(LU_ERR,'(A,A)') "Compile Date: ",TRIM(COMPILE_DATE)
       WRITE(LU_ERR,'(/A)')  "Consult FDS Users Guide Chapter, Running FDS, for further instructions."
       WRITE(LU_ERR,'(/A)')  "Hit Enter to Escape..."
